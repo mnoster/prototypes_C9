2456edb4d5df4fcfe5d1283d2097b007974a0f4e
Nicholas Porter <njporter10@gmail.com>
2016-07-01 11:18:01 -0700

    js event delegation - Nick Porter

---
+++ js_event_delegation/script.js
@@ -0,0 +1,24 @@
+// Answer the questions here:
+
+// - What do you think is going to happen?
+
+// - What happened?
+
+// - Why?
+
+//========== Write your code below ===========//
+
+$(document).ready(function(){
+//---------------this will display the # of the button clicked in the console---------------
+    $('#list button').on('click',function(){
+        console.log($(this).text());
+    });
+//----------adding a DOM element which is a button that will be added to the end of the list------------
+    var $button5 = $('<button>',{
+      appendTo: $('#list'),
+      text: "Delegated Button #5 handler"
+    });
+
+
+
+});

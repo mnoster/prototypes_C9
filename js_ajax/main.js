d1d5956d050876ad5e20def1073af08be248ecbd
Nicholas Porter <njporter10@gmail.com>
2016-07-19 14:19:41 -0700

    feature set 1 complete - Nick Porter

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,5 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result = null;
 
 $(document).ready(function(){
     $('button').click(function(){
@@ -9,9 +9,22 @@ $(document).ready(function(){
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
+                global_result = result;
+                console.log('this is global result: ', global_result);
 
+                var first_movie = 'first';
+                // iterate over each element in the array
+                for (var i = 0; i < global_result.length; i++){
+                    // look for the entry with a matching `code` value
+                    if (obj[i].code == first_movie){
+                      console.log('we found it');
+                        // obj[i].name is the matched result
+                    }
+                }
             }
         });
         console.log('End of click function');
+
     });
+
 });

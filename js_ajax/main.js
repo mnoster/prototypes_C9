1c66edcfd88f8b1d81400650fd72fc07446b64e0
Nicholas Porter <njporter10@gmail.com>
2016-07-19 22:58:46 -0700

    Nick Porter

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -11,14 +11,17 @@ $(document).ready(function(){
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
                 global_result = result;
-                console.log('this is global result: ', global_result);
-                var first_movie = global_result.feed.entry[0]['im:image'][0].label;
-                movie_list = result.feed.entry;
-                console.log('first movie img: ' + first_movie);
-                global_result = first_movie;
-                console.log('movie list length: ' + movie_list.length);
-                for(var i = 0; i< movie_list.length; i++){
-                    console.log(result.feed.entry[i]['im:image'][2])
+                var first_movie = global_result.feed.entry[0]['im:image'][0].label; //shortens the code into a new variable
+                for(var i = 0; i< movie_list.length; i++){ //for each moive
+                    var director = movies[i]['im:artist'].label;  // director variable to the label of the 'im:artist' property of the movie
+                    var title = movies[i]['im:name'].label;
+                    var all_imgs = result.feed.entry[i]['im:image'][2].label;
+                    var movie_list = result.feed.entry;
+                    var movie_image = $('<img>').attr('src', all_imgs);
+                    var new_div = $('<div id="main">');
+                    $('#main').css({'height' : '400px' , 'width': '100%','background-color':'lightgrey'}).append(all_imgs);
+
+                    console.log(all_imgs);
                 }
             }
         });

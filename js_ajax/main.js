15dd0b20377ec4a3a96c7fd83eda7907dfe7c8c2
Nicholas Porter <njporter10@gmail.com>
2016-07-19 19:20:05 -0700

    feature set 3 - Nick Porter

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,6 +1,6 @@
 //Create GLOBAL variable below here on line 2
 var global_result = null;
-
+var movie_list = null;
 $(document).ready(function(){
     $('button').click(function(){
         console.log('click initiated');
@@ -12,7 +12,13 @@ $(document).ready(function(){
                 global_result = result;
                 console.log('this is global result: ', global_result);
                 var first_movie = global_result.feed.entry[0]['im:image'][0].label;
-                console.log(first_movie);
+                movie_list = result.feed.entry;
+                console.log('first movie img: ' + first_movie);
+                global_result = first_movie;
+                console.log('movie list length: ' + movie_list.length);
+                for(var i = 0; i< movie_list.length; i++){
+                    console.log( result.feed.entry[i]['im:image'][2])
+                }
             }
         });
         console.log('End of click function');

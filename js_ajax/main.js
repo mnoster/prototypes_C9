1d48b38372b424cb8765da6154146a64573b2d4d
Nicholas Porter <njporter10@gmail.com>
2016-07-21 22:08:57 -0700

    feature set 4 - Nick Porter

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -3,26 +3,21 @@ var global_result = null;
 var movie_list = null;
 
 $(document).ready(function(){
-    $('button').click(function(){
+    $('button').one('click', function(){
         console.log('click initiated');
         $.ajax({
             dataType: 'json',
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
-                global_result = result;
-                // var first_movie = global_result.feed.entry[0]['im:image'][0].label;
                 var movie_list = result.feed.entry; //shortens the code into a new variable
-                
-                for(var i = 0; i< movie_list.length; i++){ //for each moive
-                    var director = movies[i]['im:artist'].label;  // director variable to the label of the 'im:artist' property of the movie
-                    var title = movies[i]['im:name'].label;
+                for(var i = 0; i< movie_list.length; i++){ //for each movie
+                    var director = movie_list[i]['im:artist'].label;  // director variable to the label of the 'im:artist' property of the movie
+                    var title = movie_list[i]['im:name'].label;
                     var all_imgs = result.feed.entry[i]['im:image'][2].label;
                     var movie_image = $('<img>').attr('src', all_imgs);
-                    var new_div = $('<div id="main">');
-                    $('#main').css({'height' : '400px' , 'width': '100%','background-color':'lightgrey'}).append(all_imgs);
-
-                    console.log(all_imgs);
+                    var director_titles = $('<h2>').text('Title: ' + title + ' , Director Name: ' +  director);
+                    $('#main').css({'text-align':'center'}).append(movie_image,director_titles);
                 }
             }
         });

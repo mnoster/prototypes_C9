8c239a6599e418713622879c22da24bd4bdbc1a0
Nicholas Porter <njporter10@gmail.com>
2016-08-12 12:32:52 -0700

    got a successful api response with the proper data

---
+++ ang_services_creation/script.js
@@ -0,0 +1,60 @@
+var app = angular.module('sgtApp', []);
+
+app.provider('sgtData', function () {
+    console.log("provider");
+    this.self = this;
+    var api_url = "http://s-apis.learningfuze.com/sgt/get";
+    this.$get = function ($http, $q, $log) {
+        console.log("$get");
+        return {
+            callApi: function () {
+                var data = $.param({'api_key': 'k487InnY4f'});
+                var defer = $q.defer();
+                $http({
+                    url: api_url,
+                    method: "POST",
+                    data: data,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).then(function success(response) {
+                    $log.info('success: ', response);
+                    defer.resolve(response)
+                }), function error(response) {
+                    $log.error("$http fail: ", response);
+                    defer.reject("Error msg here");
+                };
+                return defer.promise;
+            }
+        }
+
+    };
+
+
+});
+
+//Config your provider here to set the api_key and the api_url
+// app.config(function ($httpProvider){
+//    $httpProvider.defaults.headers.post =  {'Content-Type': 'application/x-www-form-urlencoded'};
+//     // $httpProvider.defaults.
+//
+// });
+
+//Include your service in the function parameter list along with any other services you may want to use
+app.controller('ioController', function (sgtData) {
+    //Create a variable to hold this, DO NOT use the same name you used in your provider
+    var new_self = this;
+    //Add an empty data object to your controller, make sure to call it 'data'
+    var data = {};
+
+    //Add a function called getData to your controller to call the SGT API
+    this.getData = function (){
+        console.log("get data fn");
+        sgtData.callApi()
+            .then(function success(response){
+                console.log("this is response: " , response);
+
+            })
+    };
+
+    //Refer to the prototype instructions for more help
+
+});

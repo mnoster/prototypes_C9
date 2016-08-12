1a7c2774ea9784a74909d299dc86ce85b6ab97a0
Nicholas Porter <njporter10@gmail.com>
2016-08-12 10:29:22 -0700

    got ajax call to be successful

--- ang_services_intro/script.js
+++ ang_services_intro/script.js
@@ -1,1 +1,62 @@
-var app = angular
+var app = angular.module("itunesApp", []);
+
+app.factory("itunesData", function($http, $q) {
+    var service = {};
+    var baseUrl = "https://itunes.apple.com/search?term=";
+    var url = '';
+    var searchTerm = '';
+    service.data = {};
+    service.error = false;
+
+    service.results = function() {
+        return service.data.results;
+    }
+
+    var makeUrl = function() {
+        url = baseUrl + searchTerm + "&callback=JSON_CALLBACK"; //the JSON_CALLBACK value is found in the ang documentation online 
+        //the &callback= can be found on the itunes api documentation
+        return url;
+    };
+
+    service.setSearch = function(s) {
+        searchTerm = s;
+        makeUrl();
+    };
+
+    service.callItunes = function() {
+        var deffer = $q.defer();
+        console.log('$q: ' , $q);
+        $http({
+            url: url,
+            method: "jsonp",
+        }).then(function(response) {
+            service.data = response.data;
+            deffer.resolve(); //defer has a method that
+        }), (function(data) {
+            service.data = data;
+            service.error = true;
+            deffer.reject();
+        });
+        console.log("defer.promise: ", deffer.promise, "deffer:", deffer);
+        return deffer.promise;
+    }
+    return service;
+});
+
+app.controller('ioController', function(itunesData) {
+    self = this;
+
+    self.results = itunesData.results;
+
+    this.updateSearch = function() {
+        itunesData.setSearch(this.searchTerm);
+    };
+
+    this.searchItunes = function() {
+        this.updateSearch();
+        itunesData.callItunes()
+            .then(function() {}, function() {
+                console.log(self.data);
+            });
+    };
+});

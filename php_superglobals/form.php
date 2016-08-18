d5e2ed428f053855de9540d3ab34843e6e1c793b
Nicholas Porter <njporter10@gmail.com>
2016-08-18 14:55:15 -0700

    added a form which will submit the data through 'get'

---
+++ php_superglobals/form.php
@@ -0,0 +1,13 @@
+<form action="index.php" method = "get">
+    <input type="text" name="name" id="my_name">
+    <select>
+        <option name = "gender">Male</option>
+        <option name = "gender">Female</option>
+        <option name = "gender">Other</option>
+
+    </select>
+    <input placeholder="Favorite Fruit" name = "fav_fruit[]">
+    <input placeholder="Favorite Fruit" name = "fav_fruit[]">
+    <input placeholder="Favorite Fruit" name = "fav_fruit[]">
+    <button>Submit</button>
+</form>
--- php_superglobals/index.php
+++ php_superglobals/index.php
@@ -2,3 +2,4 @@
 print_r($_GET);
 var_dump($_POST);
 ?>
+

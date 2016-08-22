0fec2e39de57ccfdefbfb37b65d533af6949ebb7
Nicholas Porter <njporter10@gmail.com>
2016-08-22 16:03:54 -0700

    got the data from the server to display

--- php_mysql/index_select.php
+++ php_mysql/index_select.php
@@ -1,1 +1,17 @@
 <!-- index_select.php -->
+<?php
+require_once("mysql_connect.php");
+
+
+
+$query = 'SELECT ID, name, age ,email FROM `test_table1`';
+
+$result = mysqli_query($conn,$query);
+print('<pre>' .print_r($result,true) . '</pre>');
+
+if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        print('table data: ' . '<pre>' .print_r($row, true) . '</pre>');
+    }
+}
+?>

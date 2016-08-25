d3111b626bac0f890db1418320920cbb427bf4f2
Nicholas Porter <njporter10@gmail.com>
2016-08-25 11:05:33 -0700

    feature set 1 complete

--- php_file_upload/file_handler.php
+++ php_file_upload/file_handler.php
@@ -1,1 +1,33 @@
 <!-- File Handler -->
+
+<pre>
+    <?php
+    ?>
+</pre>
+<pre>
+<?php
+print_r($_FILES['upload_file']);
+print_r($_POST);
+?>
+</pre>
+
+<?php
+
+
+$target_dir = 'uploads/'; //variable to hold target directory
+//$target_file = $target_dir . $_FILES['file_to_upload']['name'];// this will create a file path using directory
+//$uploadOK = true; //Flag used for error output
+//$output = []; //array to hold output
+//
+//
+//if (!empty($target_dir)) {
+//    pathinfo($_FILES['name']);
+//    print($_FILES['name']);
+//}
+
+?>
+<pre>
+    <?php
+    print($target_dir);
+    ?>
+</pre>
--- php_file_upload/uploader_form.php
+++ php_file_upload/uploader_form.php
@@ -3,8 +3,10 @@
 <head>
 </head>
 <body>
-	<form id="file_upload">
-		
+	<form id="file_upload" enctype="multipart/form-data" method="POST" action="file_handler.php">
+		<input type="file" name="upload_file">
+		<input name ="description" type="text">
+		<button>submit</button>
 	</form>
 </body>
 </html>

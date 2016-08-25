a5478adfb4713f76740129012263e7d38afe19d1
Nicholas Porter <njporter10@gmail.com>
2016-08-25 14:28:55 -0700

    retrieved the file extension

--- php_file_upload/file_handler.php
+++ php_file_upload/file_handler.php
@@ -1,33 +1,32 @@
 <!-- File Handler -->
-
 <pre>
     <?php
     ?>
 </pre>
 <pre>
 <?php
-print_r($_FILES['upload_file']);
-print_r($_POST);
+//print_r($_FILES['upload_file']);
+//print_r($_POST);
 ?>
 </pre>
 
 <?php
 
 
-$target_dir = 'uploads/'; //variable to hold target directory
-//$target_file = $target_dir . $_FILES['file_to_upload']['name'];// this will create a file path using directory
-//$uploadOK = true; //Flag used for error output
-//$output = []; //array to hold output
-//
-//
-//if (!empty($target_dir)) {
-//    pathinfo($_FILES['name']);
-//    print($_FILES['name']);
-//}
+$target_dir = file_get_contents('uploads/'); //variable to hold target directory
+$target_file = $target_dir . $_FILES['upload_file']['name'];// this will create a file path using directory
+$uploadOK = true; //Flag used for error output
+$output = []; //array to hold output
+
+
+
 
 ?>
 <pre>
     <?php
-    print($target_dir);
+    if (!empty('uploads')) {
+        $image_name = pathinfo($_FILES['upload_file']['name']);
+        print_r($image_name['extension']);
+    }
     ?>
 </pre>

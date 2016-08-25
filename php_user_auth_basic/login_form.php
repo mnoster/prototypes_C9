5bcde82c77844f15213d9e6142e57caf1a34e000
Nicholas Porter <njporter10@gmail.com>
2016-08-24 17:39:38 -0700

    finished feature set 3

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,40 @@
-<!--login_form.php-->
+<?php
+session_start();
+?>
+<script src="https://code.jquery.com/jquery-3.1.0.min.js"
+        integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+<form>
+    <input id="username" name="username" type="text">
+    <input id="password" name="password" type="text">
+
+</form>
+<button id="login" name="login">Login</button>
+
+<script>
+    $(document).ready(function () {
+        $('#login').on('click', function () {
+            verify_user();
+        });
+    });
+    function verify_user() {
+        var password = $('#password').val();
+        var username = $('#username').val();
+        $.ajax({
+            url: 'login_handler.php',
+            data: {
+                'username': username,
+                'password': password
+            },
+            cache: false,
+            method: 'POST',
+            dataType: 'json',
+            success: function (response) {
+                console.log("connected to server: ", response);
+            },
+            error: function (response) {
+                console.log("there was an error during login: ", response);
+            }
+        });
+    }
+
+</script>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,35 @@
-<!--login_handler.php-->
+<?php
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$encrypted_pass = sha1($password);
+$user_info = [
+    ['id' => '1', 'username' => 'bellabee', 'password' => sha1('etter')],
+    ['id' => '2', 'username' => 'handlerman', 'password' => sha1('quest554')],
+    ['id' => '3', 'username' => 'jasperjase', 'password' => sha1('peood')],
+    ['id' => '4', 'username' => 'hesterman', 'password' => sha1('fostern')],
+    ['id' => '5', 'username' => 'nochyboss', 'password' => sha1('4234')],
+];
+
+foreach ($user_info as $user) {
+    if ($username != $user['username'] || $encrypted_pass != $user['password']){
+        $output = array('success' => false);
+        $output_string = json_encode($output);
+        print ($output_string);
+        return;
+    }
+
+    if ($username == $user['username'] && $encrypted_pass == $user['password']) {
+        $_SESSION['user_id'] = $user['id'];
+        if (!empty($_SESSION['user_id'])) {
+            $output = array('success' => true, 'user_id' => $_SESSION['user_id'], 'message' => 'Welcome!');
+            $output_string = json_encode($output);
+            echo ($output_string);
+            return;
+        }
+    }
+}
+
+?>
+

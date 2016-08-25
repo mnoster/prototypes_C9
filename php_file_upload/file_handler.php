9530a0f7f062ce36be18f7f1f7fbd324be29b216
Nicholas Porter <njporter10@gmail.com>
2016-08-25 15:17:14 -0700

    got the img to save into the directory

--- php_file_upload/file_handler.php
+++ php_file_upload/file_handler.php
@@ -1,32 +1,56 @@
 <!-- File Handler -->
-<pre>
-    <?php
-    ?>
-</pre>
-<pre>
+<!--<pre>-->
 <?php
 //print_r($_FILES['upload_file']);
-//print_r($_POST);
-?>
-</pre>
-
+////print_r($_POST);
+//?>
+<!--</pre>-->
 <?php
-
-
-$target_dir = file_get_contents('uploads/'); //variable to hold target directory
+$target_dir = 'uploads/'; //variable to hold target directory
 $target_file = $target_dir . $_FILES['upload_file']['name'];// this will create a file path using directory
 $uploadOK = true; //Flag used for error output
-$output = []; //array to hold output
-
-
-
-
+$user_img_file = $_FILES['upload_file'];
 ?>
+
 <pre>
     <?php
     if (!empty('uploads')) {
         $image_name = pathinfo($_FILES['upload_file']['name']);
+//        print("img name: " ); print_r($image_name);
+
+    }
+    if($image_name['extension'] == 'gif'|| $image_name['extension'] == 'jpeg'|| $image_name['extension'] == 'jpg'|| $image_name['extension'] == 'png'){
+        print('<br>'. "Valid file type sent: ");
+        $img_name_state = true;
         print_r($image_name['extension']);
     }
+    if($user_img_file['size'] > 200000){
+        print('<br>'."File size is too big, 2MB max");
+        $img_size_state = false;
+    }
+    else{
+        $img_size_state = true;
+    }
+    if($img_name_state && $img_size_state){
+        $final_name = $target_dir . $image_name['basename'];
+        move_uploaded_file($user_img_file['tmp_name'], $final_name );
+
+    }
+
+    ?>
+
+</pre>
+
+
+
+
+
+
+
+
+
+
+<pre>
+    <?php
     ?>
 </pre>
